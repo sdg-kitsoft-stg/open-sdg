0092af3cf3b109ff12a7414ed2c8e13a5a0338b5
Vladyslav Starynets <v.starynets@kitsoft.ua>
2026-03-12 19:06:12 +0200

    SDG-53

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -146,8 +146,6 @@ var indicatorView = function (model, options) {
         $(OPTIONS.rootElement).find('.selected').css('width', '0');
     });
 
-    let fieldsChildrenCount = 0;
-
     MODEL.onSelectionUpdate.attach(function (sender, args) {
 
         if (args.selectedFields.length) {
@@ -167,10 +165,6 @@ var indicatorView = function (model, options) {
         $('.variable-selector').each(function (index, element) {
             var currentField = $(element).data('field');
             var element = $(OPTIONS.rootElement).find('.variable-selector[data-field="' + currentField + '"]');
-
-            if (element) {
-                fieldsChildrenCount++;
-            }
             // is this an allowed field:
             if (args.allowedFields.includes(currentField)) {
                 $(element).removeClass('disallowed');
@@ -183,7 +177,9 @@ var indicatorView = function (model, options) {
         });
     });
 
-    if (fieldsChildrenCount === 0) {
+    const fieldsContainer = document.querySelector('#fields');
+
+    if (fieldsContainer.children.length === 0) {
         const sidebar = document.getElementById('indicator-sidebar');
         const indicatorMain = document.querySelector('.indicator-main');
 
