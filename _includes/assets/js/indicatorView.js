5d7ed05a7c95f1d5b683b0a2578f251b67908ce1
Vladyslav Starynets <v.starynets@kitsoft.ua>
2026-03-12 19:36:56 +0200

    SDG-53

--- _includes/assets/js/indicatorView.js
+++ _includes/assets/js/indicatorView.js
@@ -177,21 +177,6 @@ var indicatorView = function (model, options) {
         });
     });
 
-    const fieldsContainer = document.querySelector('#fields');
-    console.log({
-        fieldsContainer,
-        len: fieldsContainer.children.length === 0
-    })
-
-    if (fieldsContainer.children.length === 0) {
-        const sidebar = document.getElementById('indicator-sidebar');
-        const indicatorMain = document.querySelector('.indicator-main');
-
-        sidebar.style.display = 'none';
-        indicatorMain.classList.remove('col-md-8');
-        indicatorMain.classList.add('col-md-12');
-    }
-
     MODEL.onFieldsStatusUpdated.attach(function (sender, args) {
 
         _.each(args.data, function (fieldGroup) {
--- _includes/assets/js/open-sdg-version.js
+++ _includes/assets/js/open-sdg-version.js
@@ -2,10 +2,6 @@ window.OPEN_SDG_VERSION = "2.4.0";
 
 
 const fieldsContainer = document.querySelector('#fields');
-console.log('open-sdg-version.js:', {
-    fieldsContainer,
-    len: fieldsContainer.children.length === 0
-})
 
 if (fieldsContainer.children.length === 0) {
     const sidebar = document.getElementById('indicator-sidebar');
