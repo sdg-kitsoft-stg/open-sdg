f310f5d14a6e1f6a6042ece174fc3491bb111e6a
Vladyslav Starynets <v.starynets@kitsoft.ua>
2026-03-13 10:26:44 +0200

    SDG-53

--- _includes/assets/js/open-sdg-version.js
+++ _includes/assets/js/open-sdg-version.js
@@ -1,19 +1,1 @@
 window.OPEN_SDG_VERSION = "2.4.0";
-
-
-const fieldsContainerLength = document.querySelectorAll('.variable-selector').length;
-
-
-console.log({
-    fieldsContainerLength,
-    items: document.querySelectorAll('.variable-selector')
-})
-
-if (fieldsContainerLength === 0) {
-    const sidebar = document.getElementById('indicator-sidebar');
-    const indicatorMain = document.querySelector('.indicator-main');
-
-    sidebar.style.display = 'none';
-    indicatorMain.classList.remove('col-md-8');
-    indicatorMain.classList.add('col-md-12');
-}
